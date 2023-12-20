aab3e61f1615f3723c015c4382163e8cea25781c
myFavShrimp <myfavshrimp@tuta.io>
2023-12-20 09:00:29 +0100

    Added jwt expiration calculation

--- src/handler/api/graphql/schema/public/query/use_case/login.rs
+++ src/handler/api/graphql/schema/public/query/use_case/login.rs
@@ -20,9 +20,12 @@ pub async fn perform_login(
 
     verify_password_hash(&user.password, &password)?;
 
+    let jwt_expiration = time::OffsetDateTime::now_utc().unix_timestamp()
+        + ctx.app_state.config.jwt_expiration_seconds;
+
     Ok(JwtClaims {
         sub: user.id,
-        exp: 0,
+        exp: jwt_expiration,
     }
     .encode(&ctx.app_state.config.jwt_secret)?)
 }
