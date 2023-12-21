23f5b85badd9280dc5c5358f1b681a0a4b6fbc96
myFavShrimp <myfavshrimp@tuta.io>
2023-12-21 15:59:15 +0100

    Added `userFileUpload` mutation draft

--- src/handler/api/graphql/schema/private/mutation.rs
+++ src/handler/api/graphql/schema/private/mutation.rs
@@ -1,3 +1,7 @@
+use std::io::Read;
+
+use async_graphql::Upload;
+
 use crate::database::user::User;
 
 use super::Context;
@@ -16,5 +20,39 @@ impl Mutation {
         let context = context.data::<Context>()?;
 
         Ok(use_case::user_settings::update_password(context, &new_password).await?)
+    }
+
+    async fn upload_user_file<'context>(
+        &self,
+        ctx: &async_graphql::Context<'context>,
+        path: String,
+        files: Vec<Upload>,
+    ) -> async_graphql::Result<String> {
+        let context = ctx.data::<Context>()?;
+
+        let file_name = &files.get(0).unwrap().value(ctx).unwrap().filename;
+
+        let mut current_dir = std::env::current_dir().unwrap();
+        current_dir.push(context.current_user.id.to_string());
+        current_dir.push(path);
+
+        let mut fs_builder = opendal::services::Fs::default();
+        fs_builder.root(current_dir.to_str().unwrap());
+
+        let op = opendal::Operator::new(fs_builder)?.finish();
+
+        let mut data = Vec::new();
+        files
+            .get(0)
+            .unwrap()
+            .value(ctx)
+            .unwrap()
+            .content
+            .read_to_end(&mut data)
+            .unwrap();
+
+        op.write(&file_name, data).await.unwrap();
+
+        Ok(String::new())
     }
 }
