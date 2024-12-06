d5158c4bc3062c9a7de25316a356c1ccbb48a66d
myFavShrimp <42263661+myFavShrimp@users.noreply.github.com>
2024-12-06 18:12:13 +0100

    Finished implementation of password change

--- bitsync-core/src/use_case/user_settings/update_user_password.rs
+++ bitsync-core/src/use_case/user_settings/update_user_password.rs
@@ -1,27 +1,47 @@
 use bitsync_database::{
-    database::{Database, TransactionBeginError},
+    database::{transaction::TransactionCommitError, Database, TransactionBeginError},
     entity::User,
     repository,
 };
 
-use crate::hash::{hash_password, PasswordHashCreationError};
+use crate::hash::{
+    hash_password, verify_password_hash, PasswordHashCreationError, PasswordHashVerificationError,
+};
 
 #[derive(thiserror::Error, Debug)]
-#[error("An unexpected error occurred")]
+#[error("Failed to update user password")]
 pub enum UpdateUserPasswordError {
+    PasswordHashVerification(#[from] PasswordHashVerificationError),
     PasswordHash(#[from] PasswordHashCreationError),
     Database(#[from] repository::QueryError),
     TransactionBegin(#[from] TransactionBeginError),
+    PasswordsMismatch(#[from] NewPasswordsMismatch),
+    DatabaseTransaction(#[from] TransactionCommitError),
 }
+
+#[derive(thiserror::Error, Debug)]
+#[error("The new passwords do not match")]
+pub struct NewPasswordsMismatch;
 
 pub async fn update_user_password(
     database: &Database,
     user: &User,
+    current_password: &str,
     new_password: &str,
-) -> Result<User, UpdateUserPasswordError> {
+    new_password_repeated: &str,
+) -> Result<(), UpdateUserPasswordError> {
+    verify_password_hash(&user.password, current_password)?;
+
+    if new_password != new_password_repeated {
+        Err(NewPasswordsMismatch)?;
+    }
+
     let hashed_password = hash_password(new_password)?;
 
     let mut transaction = database.begin_transaction().await?;
+    repository::user::update_password(&mut *transaction, &user.id, &hashed_password).await?;
 
-    Ok(repository::user::update_password(&mut *transaction, &user.id, &hashed_password).await?)
+    transaction.commit().await?;
+
+    Ok(())
 }
--- src/handler/frontend/user_settings.rs
+++ src/handler/frontend/user_settings.rs
@@ -1,26 +1,61 @@
 use std::sync::Arc;
 
 use axum::{
+    extract::State,
+    http::StatusCode,
     middleware::from_fn_with_state,
     response::{Html, IntoResponse},
     Router,
 };
-use axum_extra::routing::RouterExt;
+use axum_extra::{extract::Form, routing::RouterExt};
+use bitsync_core::use_case::user_settings::update_user_password::update_user_password;
+use serde::Deserialize;
 
 use crate::{
     auth::{require_login_middleware, AuthData},
-    handler::routes::GetUserSettingsPage,
+    handler::routes::{GetUserSettingsPage, PostUserSettingsChangePassword},
     presentation::templates::user_settings_page::UserSettingsPage,
     AppState,
 };
 
 pub(crate) async fn create_routes(state: Arc<AppState>) -> Router {
     Router::new()
-        .typed_get(account_page_handler)
+        .typed_get(user_settings_page_handler)
+        .typed_post(user_settings_password_change_handler)
         .route_layer(from_fn_with_state(state.clone(), require_login_middleware))
         .with_state(state)
 }
 
-async fn account_page_handler(_: GetUserSettingsPage, auth_data: AuthData) -> impl IntoResponse {
+async fn user_settings_page_handler(
+    _: GetUserSettingsPage,
+    auth_data: AuthData,
+) -> impl IntoResponse {
     Html(UserSettingsPage::from(auth_data.user).to_string())
+}
+
+#[derive(Deserialize)]
+struct ChangePasswordFormData {
+    pub current_password: String,
+    pub new_password: String,
+    pub new_password_repeated: String,
+}
+
+async fn user_settings_password_change_handler(
+    _: PostUserSettingsChangePassword,
+    State(state): State<Arc<AppState>>,
+    auth_data: AuthData,
+    Form(change_password_form_data): Form<ChangePasswordFormData>,
+) -> impl IntoResponse {
+    match update_user_password(
+        &state.database,
+        &auth_data.user,
+        &change_password_form_data.current_password,
+        &change_password_form_data.new_password,
+        &change_password_form_data.new_password_repeated,
+    )
+    .await
+    {
+        Ok(_) => StatusCode::OK.into_response(),
+        Err(e) => todo!("{:#?}", e),
+    }
 }
