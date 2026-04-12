c81c41fab9c6f13d0f13259aacce6ef2f0bb25d3
myFavShrimp <42263661+myFavShrimp@users.noreply.github.com>
2026-04-12 13:33:16 +0200

    Added dummy user actions

--- bitsync-frontend/src/pages/user_settings/users.rs
+++ bitsync-frontend/src/pages/user_settings/users.rs
@@ -15,13 +15,76 @@ impl Renderable for UsersTabContent {
 
                 div class=(crate::styles::user_settings_page::ClassName::USER_LIST) {
                     @for user in &self.users {
+                        @let popover_id = format!("user-actions-{}", user.id);
+
                         div class=(crate::styles::user_settings_page::ClassName::USER_ITEM) {
-                            span class=(crate::styles::user_settings_page::ClassName::USER_NAME) {
-                                (user.username)
+                            div class=(crate::styles::user_settings_page::ClassName::USER_INFO) {
+                                span class=(crate::styles::user_settings_page::ClassName::USER_NAME) {
+                                    (user.username)
+                                }
+                                @if user.is_admin {
+                                    span class=(crate::styles::user_settings_page::ClassName::USER_ADMIN_BADGE) {
+                                        "Admin"
+                                    }
+                                }
                             }
-                            @if user.is_admin {
-                                span class=(crate::styles::user_settings_page::ClassName::USER_ADMIN_BADGE) {
-                                    "Admin"
+
+                            button
+                                class=(crate::styles::user_settings_page::ClassName::USER_SETTINGS_BUTTON)
+                                popovertarget=(popover_id)
+                                title="User settings"
+                            {
+                                (crate::icons::ellipsis_vertical::EllipsisVertical)
+                            }
+
+                            div
+                                id=(popover_id)
+                                class=(
+                                    crate::styles::base::ClassName::CONTEXT_MENU, " ",
+                                    crate::styles::user_settings_page::ClassName::USER_CONTEXT_MENU,
+                                )
+                                popover
+                            {
+                                button
+                                    class=(
+                                        crate::styles::base::ClassName::CONTEXT_MENU_ITEM, " ",
+                                        crate::styles::modal::ClassName::DISABLED,
+                                    )
+                                    disabled
+                                {
+                                    span { "Make Admin" }
+                                }
+                                button
+                                    class=(
+                                        crate::styles::base::ClassName::CONTEXT_MENU_ITEM, " ",
+                                        crate::styles::modal::ClassName::DISABLED,
+                                    )
+                                    disabled
+                                {
+                                    span { "Reset TOTP" }
+                                }
+
+                                div class=(crate::styles::base::ClassName::CONTEXT_MENU_DIVIDER) {}
+
+                                button
+                                    class=(
+                                        crate::styles::base::ClassName::CONTEXT_MENU_ITEM, " ",
+                                        crate::styles::base::ClassName::CONTEXT_MENU_ITEM_DANGER, " ",
+                                        crate::styles::modal::ClassName::DISABLED,
+                                    )
+                                    disabled
+                                {
+                                    span { "Suspend User" }
+                                }
+                                button
+                                    class=(
+                                        crate::styles::base::ClassName::CONTEXT_MENU_ITEM, " ",
+                                        crate::styles::base::ClassName::CONTEXT_MENU_ITEM_DANGER, " ",
+                                        crate::styles::modal::ClassName::DISABLED,
+                                    )
+                                    disabled
+                                {
+                                    span { "Delete User" }
                                 }
                             }
                         }
